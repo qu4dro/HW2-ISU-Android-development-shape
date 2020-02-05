7e40359713cc074ad2474e9e509ec7b5d003345b
qu4dro <qu4dro.sl@gmail.com>
2020-02-05 18:43:57 +0800

    added example output

--- src/ru/orlovvv/Main.java
+++ src/ru/orlovvv/Main.java
@@ -3,6 +3,28 @@ package ru.orlovvv;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Circle c = new Circle(5);
+        Rect r = new Rect(4,2,3);
+        Square s = new Square(3);
+
+        print(c);
+        print(r);
+        print(s);
+
+        compareArea(c,r);
+        compareArea(s,c);
+    }
+
+    private static void print(final Shape s) {
+        System.out.printf("Values: area = %s perimeter = %s \n", s.getArea(), s.getPerimeter());
+    }
+
+    private static void compareArea(final Shape s, final Shape s2) {
+        if (s.getArea() > s2.getArea()) {
+            System.out.println("first area is bigger");
+        } else {
+            System.out.println("second area is bigger");
+        }
     }
 }
