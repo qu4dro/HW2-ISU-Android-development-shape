3d89d58592863c52d64c8a6ff950d81e6dbfb719
qu4dro <qu4dro.sl@gmail.com>
2020-02-05 18:21:34 +0800

    added circle class

---
+++ src/ru/orlovvv/Circle.java
@@ -0,0 +1,20 @@
+package ru.orlovvv;
+
+public class Circle extends Shape {
+
+    private int r;
+
+    public Circle(int r) {
+        this.r = r;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * r * r;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * r;
+    }
+}
